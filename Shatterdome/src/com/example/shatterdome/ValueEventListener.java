7f2d7a64a1d7b9d5f3d88e6939c37112cf934626
hinasakazaki <hinasakazaki@gmail.com>
2014-02-16 04:39:31 -0800

    Everyone gave up so I just did the pitch

---
+++ Shatterdome/src/com/example/shatterdome/ValueEventListener.java
@@ -0,0 +1,12 @@
+package com.example.shatterdome;
+
+import com.firebase.client.ValueEventListener;
+
+public class ValueEventListener implements ValueEventListener {
+
+	public void onCancelled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
